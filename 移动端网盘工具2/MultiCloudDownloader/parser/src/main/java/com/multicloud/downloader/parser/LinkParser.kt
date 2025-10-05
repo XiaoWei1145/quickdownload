f543d80ca6fd4ff58ae9d3546765a16bc12d26d8
XiaoWei1145 <1990578615@qq.com>
2025-10-05 21:21:29 +0800

    Delete 移动端网盘工具2/MultiCloudDownloader directory

--- 移动端网盘工具2/MultiCloudDownloader/parser/src/main/java/com/multicloud/downloader/parser/LinkParser.kt
+++
@@ -1,6 +0,0 @@
-package com.multicloud.downloader.parser
-
-interface LinkParser {
-    fun canHandle(url: String): Boolean
-    fun parse(url: String): ParsedResult?
-}
--- 移动端网盘工具2/MultiCloudDownloader/parser/src/main/java/com/multicloud/downloader/parser/ParsedResult.kt
+++
@@ -1,9 +0,0 @@
-package com.multicloud.downloader.parser
-
-data class ParsedResult(
-    val fileName: String,
-    val fileSize: Long?,
-    val directLink: String,
-    val requiresPassword: Boolean = false,
-    val extractionCode: String? = null
-)
